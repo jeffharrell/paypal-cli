d9fc35d6a28c746cf6759dbf4c3793f9918740f7
Jeff Harrell <jeff@juxtadesign.com>
2015-04-16 22:27:12 -0700

    :bomb: changes

--- api/authenticate.js
+++ api/authenticate.js
@@ -6,8 +6,8 @@ var Client = require('./Client');
 
 
 function Auth(config) {
-	this.config = config;
-	Client.call(this);
+    this.config = config;
+    Client.call(this);
 }
 
 
@@ -15,27 +15,27 @@ util.inherits(Auth, Client);
 
 
 Auth.prototype.authType = function authType() {
-	return 'Basic ' + this.token;
+    return 'Basic ' + this.token;
 };
 
 
 Auth.prototype.contentType = function contentType() {
-	return 'application/x-www-form-urlencoded; charset=utf-8';
+    return 'application/x-www-form-urlencoded; charset=utf-8';
 };
 
 
 Auth.prototype.body = function body(data) {
-	return data;
+    return data;
 };
 
 
 Auth.prototype.login = function login(data, callback) {
-	var errors = this.validate(data, ['clientId']);
+    var errors = this.validate(data, ['clientId']);
 
-	if (errors.length) {
-		callback(new Error('Required value(s): ' + errors.join(', ')));
-		return;
-	}
+    if (errors.length) {
+        callback(new Error('Required value(s): ' + errors.join(', ')));
+        return;
+    }
 
     this.token = new Buffer(data.clientId + ':' + data.secret, 'binary').toString('base64');
     this.post('/v1/oauth2/token', 'grant_type=client_credentials', callback);
--- api/payment.js
+++ api/payment.js
@@ -6,8 +6,8 @@ var Client = require('./Client');
 
 
 function Payment(config) {
-	this.config = config;
-	Client.call(this);
+    this.config = config;
+    Client.call(this);
 }
 
 
@@ -15,12 +15,12 @@ util.inherits(Payment, Client);
 
 
 Payment.prototype.create = function create(data, callback) {
-	var errors = this.validate(data, ['total', 'return_url', 'cancel_url']);
+    var errors = this.validate(data, ['total', 'return_url', 'cancel_url']);
 
-	if (errors.length) {
-		callback(new Error('Required value(s): ' + errors.join(', ')));
-		return;
-	}
+    if (errors.length) {
+        callback(new Error('Required value(s): ' + errors.join(', ')));
+        return;
+    }
 
     this.post('/v1/payments/payment', {
         intent: data.intent || 'sale',
@@ -44,24 +44,24 @@ Payment.prototype.create = function create(data, callback) {
 
 
 Payment.prototype.details = function details(data, callback) {
-	var errors = this.validate(data, ['payment-id']);
+    var errors = this.validate(data, ['payment-id']);
 
-	if (errors.length) {
-		callback(new Error('Required value(s): ' + errors.join(', ')));
-		return;
-	}
+    if (errors.length) {
+        callback(new Error('Required value(s): ' + errors.join(', ')));
+        return;
+    }
 
     this.get('/v1/payments/payment/' + data['payment-id'], null, callback);
 };
 
 
 Payment.prototype.execute = function execute(data, callback) {
-	var errors = this.validate(data, ['payment-id', 'payer-id']);
+    var errors = this.validate(data, ['payment-id', 'payer-id']);
 
-	if (errors.length) {
-		callback(new Error('Required value(s): ' + errors.join(', ')));
-		return;
-	}
+    if (errors.length) {
+        callback(new Error('Required value(s): ' + errors.join(', ')));
+        return;
+    }
 
     this.post('/v1/payments/payment/' + data['payment-id'] + '/execute', {
         payer_id: data['payer-id']
